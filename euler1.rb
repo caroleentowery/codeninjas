7ed0c2c5c7c3344b9eb4335ef37e9a5c560aaf26
icaroleen <ct@icaroleen.com>
2013-11-05 18:09:40 -0500

    Initial commit

---
+++ euler1.rb
@@ -0,0 +1,31 @@
+# This is my homework for Euler problem 1
+
+# Declares the starting point for threes
+threes = 3 
+
+# Creates an array with multiples of three
+wholeArray = Array.new [threes]
+while threes < 1000 
+	wholeArray.push(threes)
+	threes += 3
+end
+
+# Creates an array with multiples of five
+fives = 5 
+while fives < 1000 
+	wholeArray.push(fives)
+	fives += 5
+end
+
+# Pulls out the duplicates from my wholeArray
+wholeArray = wholeArray.uniq
+
+# Adds numbers in wholeArray
+total = 0
+for number in wholeArray
+#	puts number
+	total = total + number
+end
+
+# Writes total of sum of wholeArray to screen.
+puts total
