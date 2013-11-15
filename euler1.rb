f673368448fefc01178fec9563dbc5119846bc6d
icaroleen <ct@icaroleen.com>
2013-11-15 17:04:46 -0500

    Added user inputs for HW part 2

--- euler1.rb
+++ euler1.rb
@@ -1,5 +1,8 @@
 # This is my homework for Euler problem 1
 
+=begin
+#commenting out my first attempt CHT 2013-11-15
+
 # Declares the starting point for threes
 threes = 3 
 
@@ -28,3 +31,28 @@ end
 
 # Writes total of sum of wholeArray to screen.
 puts total
+
+=end
+
+# Round 2 of the hw to make it one loop instead of two
+#elminates need to pull out duplicates
+
+# Asks for user input at console
+# .chomp removes extra spaces
+
+puts "Enter end of range"
+range_end = gets.chomp
+puts "Enter the first divisor"
+divisor1 = gets.chomp
+puts "Enter the second divisor"
+divisor2 = gets.chomp
+
+# .to_i converts the input to integer
+result = 0
+for i in 1..range_end.to_i
+	if (i % divisor1.to_i == 0) || (i % divisor2.to_i == 0)
+		result += i
+	end
+end
+
+puts "Euler's problem \#1 The result is #{result}"
