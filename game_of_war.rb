eafd890457f69bd2cdb03b5673d614a9a0131dd9
icaroleen <ct@icaroleen.com>
2014-01-27 22:29:28 -0500

    Added showing spoils and bounty for tie condition

--- game_of_war.rb
+++ game_of_war.rb
@@ -20,27 +20,15 @@ suit = %w{S H D C}
 # Loops thorugh suit, then rank to create 52 cards
 suit.each do |s|
     rank.each do |r|
-      deck.push(s+r)
+      deck.push(r+s) # Makes card showing rank and suit
     end
 end
-
-# Shows the deck of cards to screen
-#puts "Initial deck of playing cards: " + deck.inspect + "\n" + "\n"
 
 # Converts index of cards to hash with integers from 0 - 51
 deck_of_playing_cards = Hash[deck.map.with_index.to_a] 
 
-# Checks that it's returning correct index value and integer / fixnum
-#puts deck_of_playing_cards['S5']  #=> 3
-#puts deck_of_playing_cards['S5'].class #=> Fixnum
-
 # Shuffles deck of cards and shows to screen
 deck.shuffle!
-#puts "Shuffled deck of playing cards: " + deck.inspect  + "\n" + "\n"
-
-# Checks that the first shuffled card output matches the correct hash value
-# puts deck[0]
-# puts deck_of_playing_cards[(deck[0])]
 
 # Initial dealing of cards, 26 for each player
 hand1 = []
@@ -50,8 +38,6 @@ hand2 = []
     hand1.push(deck.pop)
     hand2.push(deck.pop)
 end
-
-#puts "Deck after dealing is: " + deck.inspect + "\n" + "\n"
 
 # Wait for the spacebar key to be pressed
 require 'io/console'
@@ -74,15 +60,13 @@ def show_player_info(name, hand, discard_pile)
 end
 
 # Combines and shuffles hand and discard piles when less than bounty count
-def shuffle_hand(hand, discard_pile, bounty_count) 
+def rehand(hand, discard_pile, bounty_count) 
     if hand.count < bounty_count
-    hand = hand + discard_pile
-    hand.shuffle!
-    puts "Hand and discard pile were combined and reshuffled: " + hand.inspect
+        hand = (hand + discard_pile).shuffle!
+        #puts "Hand and discard pile were combined and reshuffled: " + hand.inspect
     end
+    return hand
 end
-
-
 
 # Shows initial hand and discard pile for each player at game start
 show_player_info(name1, hand1, discard_pile1)
@@ -92,26 +76,7 @@ show_player_info(name2, hand2, discard_pile2)
 until (hand1 + discard_pile1).empty? or (hand2 + discard_pile2).empty?
   
     wait_for_spacebar
-    
-    # Shows each players hand, spoils, and discard pile
-    #show_player_info(name1, hand1, spoils1, discard_pile1)
-    #show_player_info(name2, hand2, spoils2, discard_pile2)
-    
-    # Shows players' card number
-    #puts "Card number is " + deck_of_playing_cards[hand1[0]].to_s
-    
-    # Makes player skirmish
-    #player1_skirmish = deck_of_playing_cards[hand1[0]] %13
-    
-    # Shows mod value
-    #puts "Card value (mod) is " + player1_skirmish.to_s + "\n" + "\n"
-    
-    #puts "Card number is " + deck_of_playing_cards[hand2[0]].to_s
-    #player2_skirmish = deck_of_playing_cards[hand2[0]] %13
-    #puts "Card value (mod) is " + player2_skirmish.to_s + "\n" + "\n"
-    
-    # Skirmish and outputs winner 
-    
+
     # Creates and shows each players' spoils
     spoils1 = hand1.shift
     puts name1.to_s + "'s" + " spoils: " + spoils1.to_s + "\n" 
@@ -120,36 +85,56 @@ until (hand1 + discard_pile1).empty? or (hand2 + discard_pile2).empty?
   
     if deck_of_playing_cards[spoils1] %13 > deck_of_playing_cards[spoils2] %13 # Player 1 wins
         discard_pile1.push(spoils1, spoils2) # Adds both spoils to winner's hand
-        puts "* * * " + name1.to_s + " wins this skirmish. * * *\n\n" # Outputs winner name
+        puts "* * * " + name1.to_s + " wins this skirmish and takes the spoils " + spoils1 + ", " + spoils2 + " * * *\n\n" # Outputs winner name
         show_player_info(name1, hand1, discard_pile1) # Shows winner's info
         show_player_info(name2, hand2, discard_pile2) 
         
     elsif deck_of_playing_cards[spoils2] %13 > deck_of_playing_cards[spoils1] %13 # Player 2 wins
         discard_pile2.push(spoils1, spoils2)
-        puts "* * * " + name2.to_s + " wins this skirmish. * * *\n\n" # Outputs winner name
+        puts "* * * " + name2.to_s + " wins this skirmish and takes the spoils " + spoils1 + ", " + spoils2 + " * * *\n\n" # Outputs winner name
         show_player_info(name2, hand2, discard_pile2) 
         show_player_info(name1, hand1, discard_pile1)
         
     else # Tie condition
-        puts "* * * " + name1.to_s + " and " + name2.to_s + "are going to battle! * * *\n\n" # Outputs it's a battle
+        puts "* * * " + name1.to_s + " and " + name2.to_s + " are going to battle! * * *\n\n" # Outputs it's a battle
         show_player_info(name1, hand1, discard_pile1)
         show_player_info(name2, hand2, discard_pile2) 
 
-        # Sets bounty count = 4 if both players have at least 4 cards between hand and discard pile
-        if hand1.count + discard_pile1.count > 4 && hand2.count + discard_pile2.count > 4 
-            bounty_count = 4
-        else
-            # Sets bounty count as sum of hand and discard pile for player with least cards
-            bounty_count = [hand1.count + discard_pile1.count, hand2.count + discard_pile2.count].min 
+        # Sets bounty count
+        if hand1.count + discard_pile1.count > 4 && hand2.count + discard_pile2.count > 4 # Sets to 3 if minimum of 4 cards
+            bounty_count = 3 # Subtract 1 for the card to be played
+            puts "Bounty count is " + bounty_count.to_s
+        else # Sets bounty count as sum of hand and discard pile minus one for player with least cards
+            bounty_count = [hand1.count + discard_pile1.count, hand2.count + discard_pile2.count].min - 1
+            puts "Bounty count is " + bounty_count.to_s
         end
         
-        # Combines and shuffles if hand and discard < bounty count
-        shuffle_hand(hand1, discard_pile1, bounty_count)
-        shuffle_hand(hand2, discard_pile2, bounty_count)   
-        puts bounty_count
+        # Rehand if needed
+        hand1 = rehand(hand1, discard_pile1, bounty_count)
+        hand2 = rehand(hand2, discard_pile2, bounty_count)   
+        
+        bounty1 = [] # Defines bounty for battle
+        bounty2 = []
+        (bounty_count).times do # Adds cards to bounty based on bounty_count
+            bounty1.push(hand1.shift)  
+            bounty2.push(hand2.shift)
+        end
+        
+        # Battle card is card played after adding bounty
+        battle_card1 = []
+        battle_card2 = []
+        battle_card1 = hand1.shift
+        battle_card2 = hand2.shift
+       
+        # Outputs card played, bounty, and spoils
+        puts name1.to_s + " played " + battle_card1.inspect + " with bounty " + bounty1.inspect +  " to create spoils " + (spoils1 =               bounty1.push(battle_card1).inspect)
+        puts name2.to_s + " played " + battle_card2.inspect + " with bounty " + bounty2.inspect +  " to creates spoils " + (spoils2 = bounty2.push(battle_card2).inspect)
+        
+# Need to add way to check who wins this battle.  Should I re-write all that same code again?
+# Need to handle how to do another battle if needed
+# Need to add how to rehand if needed here
+
     end
- 
- 
 end 
 
 
--- test_game_of_war.rb
+++ test_game_of_war.rb
@@ -1,28 +1,65 @@
-hand1 = [1,2]
+hand1 = [1,2,3,4,5,6,7]
 discard_pile1 = [5]     
      
 hand2 = [7]
-discard_pile2 = [8, 9]     
-   def shuffle_hand(hand, discard_pile, bounty_count) 
-              if hand.count < bounty_count
-              hand = hand + discard_pile
-              hand.shuffle!
-              puts hand.inspect
-              end
-            end
+discard_pile2 = [8, 9, 10, 11]  
+
+#combined = hand1 + hand2   
+#puts combined.inspect
+
+# Combines and shuffles hand and discard piles when less than bounty count
+def rehand(hand, discard_pile, bounty_count) 
+    if hand.count < bounty_count
+    hand = (hand + discard_pile).shuffle!
+    end
+    return hand
+end
      
         if hand1.count + discard_pile1.count > 4 && hand2.count + discard_pile2.count > 4 
-          bounty_count = 4
-            shuffle_hand(hand1, discard_pile1, bounty_count)
-            shuffle_hand(hand2, discard_pile2, bounty_count)
+          bounty_count = 3
+            puts "Bounty count is " + bounty_count.to_s
         else
             # Sets bounty count as sum of hand and discard pile for player with least cards
-            bounty_count = [hand1.count + discard_pile1.count, hand2.count + discard_pile2.count].min 
-            puts bounty_count.to_s
-            shuffle_hand(hand1, discard_pile1, bounty_count)
-            shuffle_hand(hand2, discard_pile2, bounty_count)
+            bounty_count = [hand1.count + discard_pile1.count, hand2.count + discard_pile2.count].min - 1 
+            puts "Bounty count is " + bounty_count.to_s
         end
+          
+hand1 = rehand(hand1, discard_pile1, bounty_count)
+hand2 = rehand(hand2, discard_pile2, bounty_count)
+       
+       bounty1 = []
+       bounty2 = []
+        
+        puts "Hand 1 " + hand1.inspect
+        puts "Hand 2 " + hand2.inspect
+        
+         bounty_count.times do
+            bounty1.push(hand1.shift) 
+            bounty2.push(hand2.shift) 
+          end
+          
+          
+        
+        battle_card1 = []
+        battle_card2 = []
+        battle_card1 = hand1.shift
+        battle_card2 = hand2.shift
+        puts "battle card 1: " + battle_card1.to_s
+        puts "battle card 2: " + battle_card2.to_s
+
+        temp_bounty1 = bounty1
+        temp_bounty2 = bounty2
+        puts "bounty 1: " + temp_bounty1.inspect
+        puts "bounty 2: " + temp_bounty2.inspect
+        
+        spoils1 = bounty1.push(battle_card1)
+        spoils2 = bounty2.push(battle_card2)        
+        puts "1 played " + battle_card1.to_s + " and bounty is " + temp_bounty1.inspect + " with spoils " + spoils1.inspect
+        puts "2 played " + battle_card2.to_s + " and bounty is " + temp_bounty2.inspect + " with spoils " + spoils2.inspect + "\n" + "\n"
+ 
        
  # Checks if hand < bounty_count
+=begin  
+=end
  
 
